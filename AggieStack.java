3760677afc7be33132b1bfd7687fb68afde7caf5
Nathan <nateleake@tamu.edu>
2017-09-09 21:58:11 -0500

    API interface expansion

--- AggieStack.java
+++ AggieStack.java
@@ -1,20 +1,20 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class AggieStack{
+	public static void main(String... args){new AggieStack();}
 	private static AggieStack instance;
 	public static AggieStack getInstance(){return instance;}
-	public static void main(String... args){new AggieStack();}
-	private ArrayList<Machine> machines = new ArrayList<Machine>();
-	private ArrayList<Image> images = new ArrayList<Image>();
-	private ArrayList<Flavor> flavors = new ArrayList<Flavor>();
+	protected HashMap<String, Machine> machines = new HashMap<String, Machine>();
+	protected ArrayList<Image> images = new ArrayList<Image>();
+	protected HashMap<String, Flavor> flavors = new HashMap<String, Flavor>();
 
 	public AggieStack(){
 		instance = this;
 		new CommandAdmin();
 		new CommandConfig();
 		new CommandShow();
-
 		Scanner scanner = new Scanner(System.in);
 
 		while(scanner.hasNext()){
@@ -22,7 +22,7 @@ public class AggieStack{
 			if(input.toLowerCase().startsWith("aggiestack ")){
 				input = input.substring(11);
 			}
-			if(input.equals("exit")) break;
+			if(input.equals("exit") || input.equals("quit")) break;
 			
 			String cmdName = input;
 			String[] args = new String[]{};
@@ -30,7 +30,7 @@ public class AggieStack{
 			int i = input.indexOf(" ");
 			if(i != -1){
 				cmdName = input.substring(0, i);
-				args = input.substring(i).split(" ");
+				args = input.substring(i+1).split(" ");
 			}
 
 			Command cmd = Command.getCommand(cmdName);
@@ -45,17 +45,22 @@ public class AggieStack{
 				FileIO.appendString("aggiestack-log.txt", log);
 			}
 		}
-
 		scanner.close();
 	}
 	
 	void addMachine(Machine machine){
-		machines.add(machine);
+		machines.put(machine.name.toLowerCase(), machine);
+	}
+	Machine getMachine(String name){
+		return machines.get(name.toLowerCase());
 	}
 	void addImage(Image image){
 		images.add(image);
 	}
 	void addFlavor(Flavor flavor){
-		flavors.add(flavor);
+		flavors.put(flavor.name.toLowerCase(), flavor);
+	}
+	Flavor getFlavor(String name){
+		return flavors.get(name.toLowerCase());
 	}
 }
